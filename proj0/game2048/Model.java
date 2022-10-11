1c5bb8af01e49a5efba4b69d6a5b42fb404007d9
Lenssen <manasae.lenssen@gmail.com>
2022-10-11 15:50:19 +0530

    Done maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,14 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for(int i = 0; i < b.size(); i += 1){
+            for( int j = 0; j < b.size(); j +=1){
+                Tile maxtile = b.tile(i,j);
+                if(maxtile != null && maxtile.value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
